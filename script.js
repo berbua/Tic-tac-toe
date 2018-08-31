6d1fbaf448ce38636537e2fce88913152855ec59
berbua <katarzyna.berbeka@gmail.com>
2018-08-31 08:27:01 +0200

    Finish logic - full game ok but JS not efficient

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const gameBoard = (() => {
-  //module
   let boardArray = ["", "", "", "", "", "", "", "", ""];
   const boardTiles = document.querySelectorAll(".tile");
 
-  const render = () => {
+  function render() {
     boardTiles.forEach(tile => {
       tile.innerHTML = boardArray[tile.id];
     });
-  };
+  }
   render();
 
-  //clear
-  const clear = () => {
-    boardArray = ["", "", "", "", "", "", "", "", ""];
+  //clear board
+  function clear() {
+    for (let i = 0; i < boardArray.length; i++) {
+      boardArray[i] = "";
+    }
     render();
-    console.log(boardArray);
-  };
+  }
   //clear assignment to button
-  const clearGame = () => {
+  const clearButton = () => {
     const clearButton = document.getElementById("clear");
     clearButton.addEventListener("click", function() {
-      gameBoard.clear();
+      clear();
+      player.score = 0;
+      document.getElementById("player-score").innerHTML = player.score;
     });
   };
-  clearGame();
+  clearButton();
 
   //return object
   return {
@@ -81,7 +83,20 @@ const gameController = (() => {
       case gameBoard.boardArray[2] === "X" &&
         gameBoard.boardArray[4] === "X" &&
         gameBoard.boardArray[6] === "X":
-        window.alert("User won!");
+      case gameBoard.boardArray[0] === "X" &&
+        gameBoard.boardArray[3] === "X" &&
+        gameBoard.boardArray[6] === "X":
+      case gameBoard.boardArray[1] === "X" &&
+        gameBoard.boardArray[4] === "X" &&
+        gameBoard.boardArray[7] === "X":
+      case gameBoard.boardArray[2] === "X" &&
+        gameBoard.boardArray[5] === "X" &&
+        gameBoard.boardArray[8] === "X":
+        window.alert(`${player.userName} won!`);
+        player.score += 1;
+        document.getElementById("player-score").innerHTML = player.score;
+        gameBoard.clear();
+
         break;
       case gameBoard.boardArray[0] === "O" &&
         gameBoard.boardArray[1] === "O" &&
@@ -98,18 +113,30 @@ const gameController = (() => {
       case gameBoard.boardArray[2] === "O" &&
         gameBoard.boardArray[4] === "O" &&
         gameBoard.boardArray[6] === "O":
+      case gameBoard.boardArray[0] === "O" &&
+        gameBoard.boardArray[3] === "O" &&
+        gameBoard.boardArray[6] === "O":
+      case gameBoard.boardArray[1] === "O" &&
+        gameBoard.boardArray[4] === "O" &&
+        gameBoard.boardArray[7] === "O":
+      case gameBoard.boardArray[2] === "O" &&
+        gameBoard.boardArray[5] === "O" &&
+        gameBoard.boardArray[8] === "O":
         window.alert("Computer won!");
+        computerScore += 1;
+        document.getElementById("computer-score").innerHTML = computerScore;
+        gameBoard.clear();
         break;
     }
   }
 
-  let userPlay = () => {
+  const userPlay = () => {
     boardTiles.forEach(tile => {
       tile.addEventListener("click", function() {
         if (this.innerHTML === "") {
           this.innerHTML = "X";
           gameBoard.boardArray[this.id] = "X";
-          setTimeout(computerPlay, 750);
+          setTimeout(computerPlay, 250);
           setTimeout(checkResult, 750);
         } else {
           window.alert("Please select a free field");
@@ -124,23 +151,45 @@ const gameController = (() => {
 })();
 
 //players
-const playerFactory = (name => {
+const player = (() => {
   //factory
-  let userName;
-  let score;
-  let getUserName = () => {
-    const startButton = document.getElementById("start");
-    const playerNameDisplay = document.getElementById("playerNameDisplay");
-    startButton.addEventListener("click", function() {
-      gameBoard.clear();
-      userName = window.prompt("Please enter your name");
-      playerNameDisplay.innerHTML = userName;
-    });
-  };
-  getUserName();
+  const playerNameDisplay = document.getElementById("playerNameDisplay");
+  let userName = window.prompt("Please enter your name");
+  playerNameDisplay.innerHTML = userName;
+  let score = 0;
+
+  //getUserName();
   //return object
   return {
-    userName: this.userName,
-    score: this.score
+    userName: userName,
+    score: score
   };
 })();
+
+console.log(player.userName);
+
+//const player1 = playerFactory;
+
+//Player factory - stara
+// const playerFactory = (() => {
+//   //factory
+//   let userName;
+//   let score = 0;
+//   const getUserName = (() => {
+//     const startButton = document.getElementById("start");
+//     const playerNameDisplay = document.getElementById("playerNameDisplay");
+//     startButton.addEventListener("click", function() {
+//       gameBoard.clear();
+//       userNameInput = window.prompt("Please enter your name");
+//       playerNameDisplay.innerHTML = userNameInput;
+//       this.userName = userNameInput;
+//     });
+//   })();
+//   //getUserName();
+//   //return object
+//   return {
+//     userName: this.userNameInput || "Player",
+//     score: this.score || 0,
+//     getUserName: getUserName
+//   };
+// })();
